72ed24c8feea028bbe7a52695fdf71683753cc13
Xingchao Zhang <xingchaovv@gmail.com>
2020-11-06 00:12:56 +0800

    添加静态导入。

---
+++ projects/java-base/src/main/java/chao/example/lang/doimport/dostatic/NotUse.java
@@ -0,0 +1,8 @@
+package chao.example.lang.doimport.dostatic;
+
+class NotUse
+{
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
---
+++ projects/java-base/src/main/java/chao/example/lang/doimport/dostatic/Use.java
@@ -0,0 +1,11 @@
+package chao.example.lang.doimport.dostatic;
+
+import static java.lang.System.out;
+import static java.lang.Integer.*;
+
+class Use
+{
+    public static void main(String[] args) {
+        out.println(MAX_VALUE);
+    }
+}
