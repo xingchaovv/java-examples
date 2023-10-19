560bb7ee3c02b5a5b4bf7c75ff6e5d0cc66d0733
Xingchao Zhang <xingchaovv@gmail.com>
2023-10-19 19:23:25 +0800

    更新。

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 Joda-Time Java 时间类库实例、用法
 
+## Example1102
+
+StringSubstitutor 字符串替换器（变量）、字符替换工具  
+（Apache Commons Text 类库工具）
+
 ## Example6001
 
 Active MQ 用法实例
---
+++ java-base/src/main/java/Example1102.java
@@ -0,0 +1,28 @@
+import org.apache.commons.text.StringSubstitutor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * StringSubstitutor 字符串替换器（变量）、字符替换工具
+ * 参考文档：<a href="https://commons.apache.org/proper/commons-text/apidocs/org/apache/commons/text/StringSubstitutor.html">...</a>
+ */
+public class Example1102 {
+
+    public static void main(String[] args) {
+        String systemInfo = StringSubstitutor
+                .replaceSystemProperties("You are running with java.version = ${java.version} and os.name = ${os.name}.");
+        // You are running with java.version = 11.0.17 and os.name = Mac OS X.
+        System.out.println(systemInfo);
+
+        // 参数变量
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "zhang");
+        params.put("age", "18");
+        // 模板
+        String template = "Name: ${name}, Age: ${age}";
+        StringSubstitutor sub = new StringSubstitutor(params);
+        String replaced = sub.replace(template);
+        System.out.println(replaced);
+    }
+}
