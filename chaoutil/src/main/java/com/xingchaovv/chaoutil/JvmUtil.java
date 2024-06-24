2cc4badf8b46a9018e6d92e87709b5e1e3d3b0c1
Xingchao Zhang <xingchaovv@gmail.com>
2024-06-24 14:33:39 +0800

    更新。

---
+++ chaoutil/src/main/java/com/xingchaovv/chaoutil/JvmUtil.java
@@ -0,0 +1,76 @@
+package com.xingchaovv.chaoutil;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Date;
+
+public class JvmUtil {
+
+    public static class JarInfo {
+
+        private long fileSize;
+        private Date creationTime;
+        private Date modifiedTime;
+
+        public JarInfo(long fileSize, Date creationTime, Date modifiedTime) {
+            this.fileSize = fileSize;
+            this.creationTime = creationTime;
+            this.modifiedTime = modifiedTime;
+        }
+
+        public long getFileSize() {
+            return fileSize;
+        }
+
+        public Date getCreationTime() {
+            return creationTime;
+        }
+
+        public Date getModifiedTime() {
+            return modifiedTime;
+        }
+
+        @Override
+        public String toString() {
+            return "JarInfo{" +
+                    "fileSize=" + fileSize +
+                    ", creationTime=" + creationTime +
+                    ", modifiedTime=" + modifiedTime +
+                    '}';
+        }
+    }
+
+    public static JarInfo getJarInfo(Class<?> classInJar) {
+        URL classUrl = classInJar.getProtectionDomain().getCodeSource().getLocation();
+        Path classPath;
+        try {
+            classPath = Paths.get(classUrl.toURI());
+        } catch (URISyntaxException e) {
+            return null;
+        }
+
+        if (!classPath.toString().endsWith(".jar")) {
+            return null;
+        }
+        Path jarPath = classPath;
+
+        BasicFileAttributes attrs;
+        try {
+            attrs = Files.readAttributes(jarPath, BasicFileAttributes.class);
+        } catch (IOException ex) {
+            return null;
+        }
+
+        return new JarInfo(
+                attrs.size(),
+                new Date(attrs.creationTime().toMillis()),
+                new Date(attrs.lastModifiedTime().toMillis())
+        );
+    }
+
+}
---
+++ java-base/src/main/java/Example1107.java
@@ -0,0 +1,22 @@
+import com.xingchaovv.chaoutil.JvmUtil;
+
+import java.io.IOException;
+
+/**
+ * 获取当前 Class 文件信息、所在 Jar 信息
+ */
+public class Example1107 {
+
+    public static void main(String[] args) throws IOException {
+        Example1107 example1107 = new Example1107();
+        JvmUtil.JarInfo jarInfo = JvmUtil.getJarInfo(example1107.getClass());
+        System.out.println(jarInfo);
+    }
+
+/* 测试方法
+
+% java -cp "../../chaoutil/target/chaoutil-1.0-SNAPSHOT.jar:java-base-1.0-SNAPSHOT.jar" Example1107
+JarInfo{fileSize=224374, creationTime=Fri Jun 21 15:43:37 CST 2024, modifiedTime=Fri Jun 21 17:29:43 CST 2024}
+
+*/
+}
